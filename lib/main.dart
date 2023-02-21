9506ef36bf4d584316f3f12675da3098eb2410e2
Chathuranga97lj <chathuit97@gmail.com>
2023-02-21 12:35:52 +0530

    set color randomly

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,24 +16,40 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int count = 0;
+  // Color brown = Colors.brown;
+  // Color black = Colors.black;
+  // Color temp = Colors.white;
+  List<Color> colors = [Colors.white, Colors.teal, Colors.black, Colors.pinkAccent, Colors.red, Colors.purple];
+  int appBar = 0;
+  int scaffold = 0;
+  Random random = new Random();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black12,
+        backgroundColor: colors[scaffold],
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: const Color(0xff000000),
+          backgroundColor: colors[appBar],
           title: Text("$count"),
         ),
         body: MaterialButton(
-            child: Image.asset(
-                'images/dashatar.png'
+            child: Center(
+              child: Image.asset(
+                  'images/dashatar.png'
+              ),
             ),
             onPressed: (){
                setState(() {
                  count++;
+                 // temp = brown;
+                 // brown = black;
+                 // black = temp;
+                 appBar = random.nextInt(6);
+                 scaffold = random.nextInt(6);
+
                });
              },
         ),
